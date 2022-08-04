da4ef1e761a9f8193dcc3cf2ffaf37b442bd00b6
NodesXV <EShaoBruh@Erics-MBP.fios-router.home>
2022-08-04 19:22:34 -0400

    Starting the blog container

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -3,7 +3,11 @@ import './blog.css'
 
 const Blog = () => {
     return (
-        <div>
+        <div className="gpt3__blog section-padding" id="blog">
+            <div className="gpt3__blog-heading">
+            <h1 className="gradient__text">A lot is happening, <br /> We are blogging about it.</h1>
+            
+            </div>
             Blog
         </div>
     )
