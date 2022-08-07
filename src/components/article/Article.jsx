8ba32a14216076759c938b97496158dd9e132583
NodesXV <EShaoBruh@Erics-MacBook-Pro.local>
2022-08-07 16:14:12 -0400

    Updated Footer JSX and CSS

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './article.css'
 
-const Article= ({imageUrl, date, title}) => {
+const Article= ({imgUrl, date, title}) => {
     return (
         <div className="gpt3__blog-container_article">
             <div className="gpt3__blog-container_article-image">
-                <img src={imageUrl} alt="blog"/>
+                <img src={imgUrl} alt="blog"/>
             </div>
             <div className="gpt3__blog-container_article-content">
                 <div>
